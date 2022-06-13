13bc60a0088eabf006cec3566a5b15229153a99c
Jeremy Kale Padot <jeremy@padot.us>
2022-06-13 13:31:19 -0700

    add custom color to extend

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,7 +8,11 @@ module.exports = {
       lg: '976px',
       xl: '1440px',
     },
-    extend: {},
+    extend: {
+      colors: {
+        brightRed: 'hsl(12, 88%, 59%)',
+      },
+    },
   },
   plugins: [],
 }
